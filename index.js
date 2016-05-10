4eac9e192f0fe5e1100e9327c9c3b211cdea47c3
JackOfAshes <mohit.gawande@seedoc.co>
2016-05-10 12:46:45 +0530

    djnfn

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+//var querystring = require('querystring');
+var paytm = require('./sdks/paytm/checksum.js')
+
+var app = express();
+console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+ 
+app.get('*', function(req, res){
+    res.status('200').send('FUCK You')
+})
+app.post('/test', function(request, response){
+	console.log('REQUEST', request);
+	console.log('REQUEST BODY', request.body) 
+});
+var port = process.env.PORT||3000;
+
+app.listen(port);
+console.log("App listening on port "+ port);
